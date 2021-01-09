e8597cfa7d207e25bd18248513aa2edce6fba9a3
Alessio Pinna <alessiopinna@MacBook-Air-di-Alessio.local>
2021-01-09 15:37:33 +0100

    sviluppi

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import Express from "express";
+import mysql from 'mysql';
+import cors from 'cors';
+
+const app = Express();
+app.use(Express.json());
+app.use(Express.urlencoded( {extended : true} ))
+
+
+app.use(cors({
+    origin: '*'
+  }));
+
+
+  const connection = mysql.createConnection({
+    host     : 'shopux.coouthbw1pyt.eu-central-1.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'Alessio07081989',
+    database : 'learnit'
+});
+
+connection.connect((err: any) =>{
+    if(err){
+        throw err;
+    }
+    console.log("Mysql Connected ...")
+})
+
+const server = app.listen(process.env.PORT || 8000, function () {
+    console.log("Listening on port http://localhost:8000");
+});
+
+app.get('/' , (req,res) => {
+    res.send('Hello World')
+});
